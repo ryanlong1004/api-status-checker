ad3cb3eea1c9102a3491399ccdd1dc0928215c64
Ryan Long <ryan.long@noaa.gov>
2023-12-21 16:55:43 -0500

    Intitial commit

--- src/main.py
+++ src/main.py
@@ -1,20 +1,15 @@
-import logging
+import httpx
+from loguru import logger
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-    handlers=[logging.FileHandler("archive_results.log"), logging.StreamHandler()],
-)
-
-
-def add_two(value: int) -> int:
-    """adds 2 to value and returns"""
-    return value + 2
-
+ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
 
 def main():
     """main execution"""
+    logger.info("starting...")
+    r = httpx.get(ENDPOINT)
+    logger.info("complete...")
+    print(r.status_code)
 
 
 if __name__ == "__main__":
-    pass
+    main()
