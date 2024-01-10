512813ca406e4cc6ed3c89c9a3cbd01ac6d0638e
Ryan Long <ryan.long@noaa.gov>
2024-01-10 19:33:00 +0000

    bug fixes and comments

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import socket
-from typing import Any
+from typing import Any, List
 import json
 import httpx
 from loguru import logger
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+EMAIL_TO = os.getenv("STATUS_CHECKER_EMAIL")
 
 logger.add(
     "/usr1/rlong/api-status-checker/LOG",
@@ -27,10 +28,13 @@ HEADERS = {
 
 
 async def to_job(fx, *params):
+    """enables regular functions to run async"""
     return await asyncio.to_thread(fx, *params)
 
 
 class CheckResult:
+    """represents the results of a url check"""
+
     def __init__(self, name, url, status_code, content):
         self.name = name
         self.url = url
@@ -39,34 +43,45 @@ class CheckResult:
 
     @property
     def result(self):
+        """returns the result as eith 'PASS' or 'FAIL'"""
         return "PASS" if self.status_code == 200 else "FAIL"
 
     def __str__(self):
         return f"{self.result}: {self.name} {self.content if self.content else ''}"
 
 
-def check_endpoint(name: str, url: str, headers: dict[str, Any]):
+def check_endpoint(
+    name: str, url: str, headers: dict[str, Any], client
+) -> "CheckResult":
+    """hits the url with a get request and returns the results as a CheckResult"""
     try:
-        response = httpx.get(url, headers=headers, timeout=10)
-        result = CheckResult(
+        response = client.get(url, headers=headers, timeout=10)
+        return CheckResult(
             name,
             url,
             response.status_code,
             response.content if response.status_code != 200 else None,
         )
-        if result.status_code != 200:
-            logger.error(result)
-            email(
-                os.getenv("STATUS_CHECKER_EMAIL"),
-                f"API Failure {datetime.datetime.now().isoformat()}",
-                str(result),
-            )
-        logger.info(result)
 
     except (socket.timeout, httpx.ReadTimeout) as e:
-        result = CheckResult(name, url, 1, str(e))
-        logger.error(CheckResult(name, url, 1, str(e)))
-        email(os.getenv("STATUS_CHECKER_EMAIL"), "API Failure", str(result))
+        return CheckResult(name, url, 408, str(e))
+
+
+def handle_check_results(results: List[CheckResult]):
+    """logs the results and sends an email of all failed results"""
+    errors = []
+    for result in results:
+        message = f"{result.status_code}:{result}"
+        if result.status_code != 200:
+            logger.error(message)
+            errors.append(message)
+            continue
+        logger.info(message)
+    email(
+        EMAIL_TO,
+        f"API Failure {datetime.datetime.now().isoformat()}",
+        "\n".join(errors),
+    )
 
 
 async def main():
@@ -76,12 +91,15 @@ async def main():
         "/usr1/rlong/api-status-checker/queue.json", "r", encoding="utf-8"
     ) as _input:
         endpoints = json.load(_input)
-        await asyncio.gather(
-            *[
-                to_job(check_endpoint, name, endpoint, HEADERS)
-                for name, endpoint in endpoints.items()
-            ]
-        )
+        results = []
+        with httpx.Client() as client:
+            results = await asyncio.gather(
+                *[
+                    to_job(check_endpoint, name, endpoint, HEADERS, client)
+                    for name, endpoint in endpoints.items()
+                ]
+            )
+        handle_check_results(results)
         logger.info("finished")
 
 
